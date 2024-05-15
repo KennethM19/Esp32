e2f0f94910fd2dc8050c50f77f4b70302197b35f
Kenneth19M <ken.jav550@gmail.com>
2024-05-15 07:05:54 -0500

    firstcommit

---
+++ config/Database.php
@@ -0,0 +1,24 @@
+<?php
+
+class dataBase
+{
+    private $host = "localhost";
+    private $dbname = "esp32";
+    private $user = "root";
+    private $pass = "";
+
+    public function connect()
+    {
+        try {
+            $PDO = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->pass);
+            return $PDO;
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+
+    }
+
+}
+
+
+?>
